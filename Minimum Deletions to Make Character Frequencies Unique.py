dcdb709cfda95c881d5a8e4cefbdc3a353908ed8
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-27 21:04:24 -0700

    Add files via upload

---
+++ Minimum Deletions to Make Character Frequencies Unique.py
@@ -0,0 +1,17 @@
+# 1647. Minimum Deletions to Make Character Frequencies Unique
+
+# A string s is called good if there are no two different characters in s that have the same frequency.
+
+# Given a string s, return the minimum number of characters you need to delete to make s good.
+
+# The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        cnt, res, used = collections.Counter(s), 0, set()
+        for ch, freq in cnt.items():
+            while freq > 0 and freq in used:
+                freq -= 1
+                res += 1
+            used.add(freq)
+        return res
